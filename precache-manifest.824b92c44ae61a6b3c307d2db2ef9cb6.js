5c5392f8c7a230e27c9bbcf15d1705f978d804e9
Jayadeep Pulikallu <jayadeeppulikallu@Jayadeeps-MacBook-Pro.local>
2020-06-16 11:41:49 +0530

    Updates

---
+++ precache-manifest.824b92c44ae61a6b3c307d2db2ef9cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58e822368a11fc2035d00df79a5958b",
+    "url": "/chinnu/index.html"
+  },
+  {
+    "revision": "0e4129057c9f42e4e353",
+    "url": "/chinnu/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "6aa01ce370e97d9c40c0",
+    "url": "/chinnu/static/css/main.76ff40f4.chunk.css"
+  },
+  {
+    "revision": "0e4129057c9f42e4e353",
+    "url": "/chinnu/static/js/2.aed92faf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chinnu/static/js/2.aed92faf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6aa01ce370e97d9c40c0",
+    "url": "/chinnu/static/js/main.c71aa7b0.chunk.js"
+  },
+  {
+    "revision": "c466c13661721539dbd3",
+    "url": "/chinnu/static/js/runtime-main.a71a4ad9.js"
+  }
+]);
